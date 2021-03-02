f3923c5717e9e76a963559d3289f863dd88a47dd
giandata <happydaysbcn2017@gmail.com>
2021-03-02 22:27:09 +0100

    specified engine

--- forecastapp.py
+++ forecastapp.py
@@ -30,7 +30,7 @@ page = st.sidebar.radio("Tabs",tabs)
 def load_csv():
     
     df_input = pd.DataFrame()  
-    df_input=pd.read_csv(input,sep=None , encoding='utf-8',
+    df_input=pd.read_csv(input,sep=None ,engine='python', encoding='utf-8',
                             parse_dates=True,
                             infer_datetime_format=True)
     return df_input

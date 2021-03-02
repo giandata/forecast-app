75b98c3ce7d0f6ad65f436d0a8c55228b3f04c67
giandata <happydaysbcn2017@gmail.com>
2021-03-02 21:45:05 +0100

    dict unpacking best tuning

--- forecastapp.py
+++ forecastapp.py
@@ -60,7 +60,7 @@ if page == "Application":
     
     if input is None:
         st.write("Or use sample dataset to try the application")
-        sample = st.checkbox("download sample data from GitHub")
+        sample = st.checkbox("Download sample data from GitHub")
 
     try:
         if sample:
